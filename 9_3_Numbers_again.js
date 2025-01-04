d382ea63e913cef5078f93ec800aee35de1797b1
NamHey99 <112053839+NamHey99@users.noreply.github.com>
2025-01-04 09:58:55 +0700

    Create 9_3_Numbers_again.js
    Check correct number or not

---
+++ 9_3_Numbers_again.js
@@ -0,0 +1,18 @@
+// Fill in this regular expression.
+let number = /^...$/;
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12"]) {
+  let num = Number(str);
+  if (isNaN(num)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+                 ".5.", "1f5", "."]) {
+  let num = Number(str);
+  if (!isNaN(num)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
